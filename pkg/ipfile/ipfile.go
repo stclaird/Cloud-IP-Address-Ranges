5ce34b49c7ea793a60f5134ca93ca1832804ad9a
David StClair <david@davidstclair.co.uk>
2023-08-31 14:32:31 +0100

    Improvement to buffer when reading cloud files

--- pkg/ipfile/ipfile.go
+++ pkg/ipfile/ipfile.go
@@ -130,6 +130,8 @@ func AsText[T any](DownloadFileName string) []string {
 
 	var cidrs []string
 	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 0, 64*1024)
+    scanner.Buffer(buf, 1024*1024)
 
 	for scanner.Scan() {
 		txt := scanner.Text()
